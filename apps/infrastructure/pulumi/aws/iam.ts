2a0552775c7202e1d5f0b11c59ce34388e4a10de
devunt <337728+devunt@users.noreply.github.com>
2024-10-26 07:37:11 +0000

    aws/k8s 인증 방식 정리 (#1316)

--- apps/infrastructure/pulumi/aws/iam.ts
+++ apps/infrastructure/pulumi/aws/iam.ts
@@ -74,6 +74,27 @@ new aws.iam.UserPolicy('developer@team', {
   },
 });
 
+const adminRole = new aws.iam.Role('admin@team', {
+  name: 'admin@team',
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+    AWS: '886436942314',
+  }),
+});
+
+new aws.iam.RolePolicy('admin@team', {
+  role: adminRole.name,
+  policy: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Action: '*',
+        Resource: '*',
+      },
+    ],
+  },
+});
+
 const developerAccessKey = new aws.iam.AccessKey('developer@team', {
   user: developer.name,
 });
