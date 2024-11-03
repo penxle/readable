6cf5a4550ffed9f76dc71dd22dee8efc30363570
devunt <337728+devunt@users.noreply.github.com>
2024-11-03 07:34:27 +0000

    불필요해진 workaround 제거 (#1344)
    업스트림에서 버그 수정됨

--- packages/ui/src/tiptap/components/TiptapEditor.svelte
+++ packages/ui/src/tiptap/components/TiptapEditor.svelte
@@ -55,13 +55,6 @@
         attributes: { class: css(style) },
         scrollMargin: { top: 150, bottom: 50, left: 0, right: 0 },
         scrollThreshold: { top: 150, bottom: 50, left: 0, right: 0 },
-        handleKeyDown: (_, event) => {
-          // 맥 구름입력기에서 엔터키 입력시 마지막 글자 잘리는 문제 workaround
-          if (editor && event.key === 'Enter') {
-            const s = editor.view.state.selection;
-            editor.commands.setTextSelection(s.to);
-          }
-        },
         handleDrop: (view, event) => {
           if (event.dataTransfer?.files?.length) {
             dispatch('file', {
