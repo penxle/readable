df9f5eeed8874a31ccfeb00335de1fea0af2e1b8
Seohyun-Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2024-11-12 09:04:52 +0000

    위젯 mixpanel init (#1430)

--- sdks/browser-sdk/src/script.ts
+++ sdks/browser-sdk/src/script.ts
@@ -1,6 +1,13 @@
 import './App.svelte';
 
+import Mixpanel from 'mixpanel-browser';
 import styles from './app.css?inline';
+
+Mixpanel.init(import.meta.env.DEV ? '70816f3a6b13fa5f4c0dad59690c00bc' : '9bce2be6e6c87eeaa28d9a1bc413e4d8', {
+  api_host: 'https://t.rdbl.app',
+  ignore_dnt: true,
+  persistence: 'localStorage',
+});
 
 const siteId = (document.currentScript as HTMLScriptElement).dataset.siteId;
 if (!siteId) {
