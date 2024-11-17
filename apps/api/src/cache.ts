2885834701d574b9af974bdb4ffa8963b77ed395
devunt <337728+devunt@users.noreply.github.com>
2024-11-17 08:32:45 +0000

    cached 유틸리티 추가 (#1463)

--- apps/api/src/cache.ts
+++ apps/api/src/cache.ts
@@ -1,4 +1,17 @@
 import { Redis } from 'ioredis';
+import superjson from 'superjson';
 import { env } from '@/env';
 
 export const redis = new Redis(env.REDIS_URL);
+
+export const cached = async <T>(key: string, ttl: number, fn: () => Promise<T>) => {
+  const value = await redis.get(key);
+  if (value) {
+    return superjson.parse(value);
+  }
+
+  const v = await fn();
+  await redis.setex(key, ttl, superjson.stringify(v));
+
+  return v;
+};
