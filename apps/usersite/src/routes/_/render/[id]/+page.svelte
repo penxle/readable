488f8d96ed6ad3e79b378c4463b88a754f9afb50
devunt <337728+devunt@users.noreply.github.com>
2024-11-19 06:54:39 +0000

    유저사이트 렌더 프레임 탈출 버그 수정 (#1483)

--- apps/usersite/src/routes/_/render/[id]/+page.svelte
+++ apps/usersite/src/routes/_/render/[id]/+page.svelte
@@ -21,13 +21,20 @@
       }
     }
   `);
+
+  const handleClick = (e: MouseEvent) => {
+    if (e.target instanceof HTMLAnchorElement) {
+      e.preventDefault();
+      window.open(e.target.href, '_blank');
+    }
+  };
 </script>
 
-<svelte:head>
-  <base target="_blank" />
-</svelte:head>
-
-<div style:--usersite-theme-color={$query.publicSite.themeColor} class={css({ display: 'contents' })}>
+<div
+  style:--usersite-theme-color={$query.publicSite.themeColor}
+  class={css({ display: 'contents' })}
+  onclickcapture={handleClick}
+>
   <h1 class={css({ textStyle: '34b', marginBottom: '32px' })}>{$query.publicPageById.title}</h1>
   <TiptapRenderer content={$query.publicPageById.content.content} />
 </div>
