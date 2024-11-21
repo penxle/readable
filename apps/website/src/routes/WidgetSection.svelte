b57daa62b815c0bc40726f8bffba40e8b2ce942e
devunt <337728+devunt@users.noreply.github.com>
2024-11-21 09:08:52 +0000

    랜딩 자잘한 문구 수정 (#1493)

--- apps/website/src/routes/WidgetSection.svelte
+++ apps/website/src/routes/WidgetSection.svelte
@@ -34,8 +34,9 @@
       {#snippet description()}
         <p>
           <span>
-            유저가 보고 있는 내용을 이해하고 가이드 문서를 제안해줍니다. <br />
-            물론, 문서 내용을 확인하거나 물어보는 것도 가능합니다.
+            이용자가 어려움을 겪는 그 순간, 가장 적합한 도움말을 자동으로 실시간 제안합니다.
+            <br />
+            페이지를 떠나지 않고도 서비스 내에서 바로 도움말을 제공해보세요.
           </span>
         </p>
       {/snippet}
@@ -74,11 +75,12 @@
         })}
       >
         <h2 class={css({ fontSize: '22px', fontWeight: '[700]', smOnly: { fontSize: '18px' } })}>
-          관련 문서 자동 추천
+          맥락을 이해하는 문서 제안
         </h2>
         <p class={css({ marginTop: '4px', fontSize: '16px', fontWeight: '[500]', smOnly: { fontSize: '14px' } })}>
-          유저가 보고 있는 내용을 이해하고 <br />
-          가장 필요한 가이드 문서를 제안해줍니다
+          지금 이용자가 보고 있는 내용을 이해하고
+          <br />
+          가장 필요한 가이드 문서를 자동으로 제안합니다
         </p>
         <img
           class={css({
@@ -108,7 +110,7 @@
         <h2 class={css({ fontSize: '22px', fontWeight: '[700]', smOnly: { fontSize: '18px' } })}>AI 문의</h2>
         <p class={css({ marginTop: '4px', fontSize: '16px', fontWeight: '[500]', smOnly: { fontSize: '14px' } })}>
           도움센터를 통째로 이해하는 AI 문의를 통해 <br />
-          고객의 복잡한 질문을 빠르게 해결할 수 있습니다
+          어떤 질문이든 서비스 내에서 빠르게 해결할 수 있습니다
         </p>
         <img
           class={css({
